4c70f0fc9ceb5c4c565b4631cd1d4bf0dd429c6c
Anas <muhd.anas@outlook.com>
2025-12-28 15:53:35 +0800

    fixed test

--- main_test.go
+++ main_test.go
@@ -3,7 +3,7 @@ package main
 import "testing"
 
 func TestHelloWorld(t *testing.T) {
-	expected := "Hello, World"
+	expected := "Hello, World!"
 	actual := HelloWorld()
 	if actual != expected {
 		t.Errorf("HelloWorld() = %q; want %q", actual, expected)
