9abadd093e22e717d79f4c65e041eba68c35cd75
Anas <muhd.anas@outlook.com>
2025-12-30 13:33:02 +0800

    implement server

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func HelloWorld() string {
 	return "Hello, World!"
 }
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, HelloWorld())
+}
+
 func main() {
-	fmt.Println(HelloWorld())
+	http.HandleFunc("/", helloHandler)
+	fmt.Println("Server starting on :3000...")
+	if err := http.ListenAndServe(":3000", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
 }
